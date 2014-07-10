14fae3486d90f387b518715297f763bc2313407e
wooojtek <wbabinski@gmail.com>
2014-07-10 11:09:42 +0100

    product update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+    def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
 end
