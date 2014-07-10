cee85a9e04e1db97d90f2ff2a12698266ae12fd9
wooojtek <wbabinski@gmail.com>
2014-07-10 17:28:02 +0100

    Unit Testing of Models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,9 @@
 class Product < ActiveRecord::Base
-    def change
-    create_table :products do |t|
-      t.string :title
-      t.text :description
-      t.string :image_url
-      t.decimal :price, precision: 8, scale: 2
-
-      t.timestamps
-    end
-  end
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
