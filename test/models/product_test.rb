8102c9c4523f0e95f626bd66e340b218d5fa923d
wooojtek <wbabinski@gmail.com>
2014-07-11 14:46:36 +0100

    Validation :length checks

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -15,9 +15,9 @@ class ProductTest < ActiveSupport::TestCase
   
   test "product price must be positive" do
     product = Product.new(title: "My Book Title",
-    description: "yyy",
-    image_url: "zzz.jpg")
-    product.price = -1
+                          description: "yyy",
+                          image_url: "zzz.jpg")
+                          product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
     product.errors[:price]
