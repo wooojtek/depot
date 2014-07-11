ff009d677fa8340164153a05ac3a89a15d7328e8
wooojtek <wbabinski@gmail.com>
2014-07-11 12:28:39 +0100

    Validation and Unit Testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
+I18n.enforce_available_locales = false
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -42,6 +45,15 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
-    
   end
+   
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+    
 end
