4e12acdecbb78e033a183d0a2a87f4ca72dfebde
wooojtek <wbabinski@gmail.com>
2014-07-17 17:20:07 +0100

    Selecting the Locale

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,19 @@ Depot::Application.routes.draw do
 
   get "sessions/create"
   get "sessions/destroy"
+
   resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
 
   get "store/index"
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
