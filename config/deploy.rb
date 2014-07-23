3a4ed76ae90c47cbc3e49ac0d5df66d71cfb6a9f
wooojtek <wbabinski@gmail.com>
2014-07-23 09:20:10 +0100

    capistrano config 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,37 @@ set :rvm_type, :user
 set :rvm_ruby_string, 'ruby-2.1.1-p76'
 require 'rvm/capistrano'
 
+# file paths
 set :repository,  "#{user}@#{domain}:git/#{application}.git"
+set :deploy_to, "/home/#{user}/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
 role :db,  "your slave db-server here"
+
+# you might need to set this if you aren't seeing password prompts
+# default_run_options[:pty] = true
+
+# As Capistrano executes in a non-interactive mode and therefore doesn't cause
+# any of your shell profile scripts to be run, the following might be needed
+# if (for example) you have locally installed gems or applications. Note:
+# this needs to contain the full values for the variables set, not simply
+# the deltas.
+# default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin'
+# default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
+
+# miscellaneous options
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :normalize_asset_timestamps, false
+set :rails_env, :production
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -32,3 +54,15 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+    task :restart do
+      run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "reload the database with seed data"
+      task :seed do
+      deploy.migrations
+      run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
