51965cdf4a0c9950c31e9ae198ec0dfbf4194df2
wooojtek <wbabinski@gmail.com>
2014-07-24 13:45:31 +0100

    email validation added

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,10 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
+  validates :email, allow_blank: true, format: {
+    with: %r{\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z}i,
+    message: 'must be a valid e-mail address.'
+  }
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
